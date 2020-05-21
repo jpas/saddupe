1d2c517ae641b2e8d9f4199f6a4f92f716d3f49e
Jarrod Pas <jarrod@jarrodpas.com>
2020-05-21 14:31:35 -0600

    Add MAC parsing

--- hid/internal/l2/l2.go
+++ hid/internal/l2/l2.go
@@ -9,32 +9,50 @@ import (
 
 // Addr implements net.Addr for L2CAP Addresses
 type Addr struct {
-	MAC [6]byte
+	MAC *MAC
 	PSM uint16
 }
 
 // NewAddr returns an Addr for L2CAP
 func NewAddr(mac string, psm uint16) (*Addr, error) {
-	if len(mac) != 17 {
-		return nil, errors.Errorf("invalid MAC address: %s", mac)
-	}
-	hw, err := net.ParseMAC(mac)
+	m, err := ParseMAC(mac)
 	if err != nil {
 		return nil, err
 	}
-	a := &Addr{PSM: psm}
-	for i := range a.MAC {
-		a.MAC[i] = hw[i]
-	}
-	return a, nil
+	return &Addr{m, psm}, nil
 }
 
 func (a *Addr) String() string {
-	hw := net.HardwareAddr(a.MAC[:]) // does propper string formatting for us
-	return fmt.Sprintf("[%s]:%d", hw, a.PSM)
+	return fmt.Sprintf("[%s]:%d", a.MAC, a.PSM)
 }
 
 // Network returns the "network" for L2CAP connections
 func (a *Addr) Network() string {
 	return "l2cap"
+}
+
+type MAC [6]byte
+
+func ParseMAC(mac string) (*MAC, error) {
+	hw, err := net.ParseMAC(mac)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse MAC address")
+	}
+	if len(hw) != 6 {
+		return nil, errors.Errorf("invalid MAC address: %s", mac)
+	}
+	var m MAC
+	for i := 0; i < 6; i++ {
+		m[i] = hw[i]
+	}
+
+	return &m, nil
+}
+
+func (m MAC) Bytes() []byte {
+	return m[:]
+}
+
+func (m MAC) String() string {
+	return net.HardwareAddr(m.Bytes()).String()
 }
